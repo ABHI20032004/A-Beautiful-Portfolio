46a9cdb9d9eae09f73f439da7f3d907f7d722f9b
ABHI20032004 <2024kuad3015@iiitkota.ac.in>
2025-10-21 20:04:26 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import { NotFound } from "./pages/NotFound"
 import { Toaster } from "@/components/ui/toaster";
-import { Home } from "./pages/home";
+import { Home } from "./pages/Home";
 
 function App() {
 
